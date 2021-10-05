88b36f4d711f71346746d12fdea350c3684b97b8
Francesca Aguila <francescaaguila@Francescas-MacBook-Pro.local>
2021-10-05 17:51:39 -0400

    remove unnecessary script

--- pysentiment2/sumtest.py
+++
@@ -1,9 +0,0 @@
-#
-# text_file = open("static/NegationWords.txt", "r")
-# lines = text_file.read().splitlines()
-# lines = [x.lower() for x in lines]
-# print(lines)
-import numpy as np
-b = [1,2,3,4,5]
-print(b[b > 2])
-# np.sum(b[b > 2])
